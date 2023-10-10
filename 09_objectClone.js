8f16375c6bff59699ada6e5556fb1d725fd0ade7
achyutchoudhari <achyutchoudhri@gmail.com>
2023-10-10 07:13:07 +0530

    object clone
    clone

---
+++ 09_objectClone.js
@@ -0,0 +1,15 @@
+
+
+let bankSbi = {
+   holderName :`achyut`,
+    address: `pune hinjevadi ` ,
+    accountNumber:9689038174,
+    accountType : current ,
+    age:30, 
+}
+console.log(`Creating the object bankLocation `);
+const banksbi= {
+    street:"Hinjewadi",
+    city: "Pune",
+    pin: 415015
+};
